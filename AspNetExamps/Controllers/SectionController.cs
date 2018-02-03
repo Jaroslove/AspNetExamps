aa0472944741eb68f298944adaec6f4a8b6c63fb
Jaroslove <jroslovemonro@gmail.com>
2018-02-03 23:37:13 +0300

    child actions

--- AspNetExamps/Controllers/SectionController.cs
+++ AspNetExamps/Controllers/SectionController.cs
@@ -13,5 +13,18 @@ namespace AspNetExamps.Controllers
         {
             return View();
         }
+
+        [ChildActionOnly] // start only as partial view
+        public string CurrentDate()
+        {
+            return DateTime.Now.ToLongDateString();
+        }
+
+        public ActionResult ShowTable(int number = 5)
+        {
+            IEnumerable<Models.ForView.Product> products = Models.ForView.Products.GetProducts().Take(number);
+
+            return View("_ShowTable", products);
+        }
     }
 }
---
+++ AspNetExamps/Models/ForView/Products.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AspNetExamps.Models.ForView
+{
+    public class Products
+    {
+        public static List<Product> GetProducts()
+        {
+            List<Product> list = new List<Product>();
+
+            for (int i = 0; i < 20; i++)
+            {
+                list.Add(new Product(i, $"name {i}"));
+            }
+
+            return list;
+        }
+    }
+}
