14c695dc926729a811812cd47b08048d313d3374
Jaroslove <jroslovemonro@gmail.com>
2018-02-06 21:47:18 +0300

    Added user controller with name is User

---
+++ AspNetExamps/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace AspNetExamps.Controllers
+{
+    public class UserController : IController
+    {
+        public void Execute(RequestContext requestContext)
+        {
+            string controller = requestContext.RouteData.Values["controller"].ToString();
+
+            string action = requestContext.RouteData.Values["action"].ToString();
+
+            requestContext.HttpContext.Response.Write($"Controller is {controller} and action is {action}");
+        }
+    }
+}
