c74efa73543638daf94c3614d44814beb89d49f1
Jaroslove <jroslovemonro@gmail.com>
2018-02-12 21:03:40 +0300

    Simple form

---
+++ AspNetExamps/Controllers/FormsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace AspNetExamps.Controllers
+{
+    public class FormsController : Controller
+    {
+        // GET: Forms
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
