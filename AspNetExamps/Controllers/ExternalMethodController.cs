dbcbe0cf227b4ae5c57edf8a955c8165d9bcf29c
Jaroslove <jroslovemonro@gmail.com>
2018-02-12 20:42:17 +0300

    Added external method

---
+++ AspNetExamps/Controllers/ExternalMethodController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetExamps.Controllers
+{
+    public class ExternalMethodController : Controller
+    {
+        // GET: ExternalMethod
+        public ActionResult Index()
+        {
+            ViewBag.Days = new string[] { "One", "Two"};
+
+            return View();
+        }
+    }
+}
---
+++ AspNetExamps/Infastructure/CustomHelperMethods.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetExamps
+{
+    public static class CustomHelperMethods
+    {
+        public static MvcHtmlString Listr(this HtmlHelper helper, string[] items)
+        {
+            TagBuilder tag = new TagBuilder("ul");
+
+            foreach (var item in items)
+            {
+                TagBuilder liTag = new TagBuilder("li");
+                liTag.SetInnerText(item);
+                tag.InnerHtml += liTag.ToString();
+            }
+
+            return new MvcHtmlString(tag.ToString());
+        }
+    }
+}
