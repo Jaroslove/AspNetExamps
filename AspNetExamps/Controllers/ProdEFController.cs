2449958a1dffea442d047d9a2901ef7f12758a3e
Jaroslove <jroslovemonro@gmail.com>
2018-02-13 21:46:16 +0300

    Added first data via ef

--- AspNetExamps/Controllers/ProdEFController.cs
+++ AspNetExamps/Controllers/ProdEFController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AspNetExamps.Models.EF;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,6 +15,24 @@ namespace AspNetExamps.Controllers
         public ActionResult Index()
         {
             return View(db.Products.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return null;
         }
 
         protected override void Dispose(bool disposing)
