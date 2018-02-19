fc6c71b392ee4db71f1d99302ce09ce7c5d67fce
Jaroslove <jroslovemonro@gmail.com>
2018-02-19 23:42:37 +0300

    binding simple

---
+++ AspNetExamps/Controllers/STBindingController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetExamps.Controllers
+{
+    public class STBindingController : Controller
+    {
+        // GET: STBinding
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ActionOne(int id)
+        {
+            ViewBag.Id = id;
+            return View();
+        }
+
+        public ActionResult ActionTwo(int id = 1)
+        {
+            return View("ActionOne");
+        }
+
+        public ActionResult ActionTree(int? id)
+        {
+            return View("ActionOne");
+        }
+    }
+}
