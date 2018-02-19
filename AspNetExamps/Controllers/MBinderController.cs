a57e80fa035580d6b36d895864d3711fc2cc3375
Jaroslove <jroslovemonro@gmail.com>
2018-02-20 00:25:49 +0300

    error in MBinder

---
+++ AspNetExamps/Controllers/MBinderController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AspNetExamps.Controllers
+{
+    public class MBinderController : Controller
+    {
+        // GET: MBinder
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SetData()
+        {
+            AspNetExamps.Models.MBModel.Model model = new Models.MBModel.Model();
+
+            UpdateModel(model);
+
+            return View("Index");
+        }
+    }
+}
